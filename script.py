0ad1524c57309cc1617c13aab3dbd7cbff103771
Alex Lai <alexlai913@gmail.com>
2019-01-15 19:26:08 -0800

    added location, date and time

--- script.py
+++ script.py
@@ -7,7 +7,9 @@ for img in os.listdir('images'):
     if img.endswith('.jpg'):
         fn, fext = os.path.splitext(img)
         loc = fn[0]
-        time = f[1:]
-        outfile.write(ana(fn) + loc + ',' + time + '\n')   
+        sep = fn.find('T')
+        date = fn[1:sep]
+        time = fn[sep+1:]
+        outfile.write( loc + ',' + date + ',' + time + '\n' + ana(fn) + '\n')   
 
 outfile.close()
