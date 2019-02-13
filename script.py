c5c0455e2cc5f28657444cf408ae4d4e25e0a0ef
Alex Lai <alexlai913@gmail.com>
2019-02-12 21:07:16 -0800

    added method to not reprocess images

--- script.py
+++ script.py
@@ -4,14 +4,16 @@ import os
 
 df = pd.DataFrame([],columns = ['Location','Date','Time','People'])
 
+
 while True:
-        for img in os.listdir('images'):
-                if img.endswith('.jpg'):
-                        fn, fext = os.path.splitext(img)
-                        loc = fn[0]
-                        sep = fn.find('T')
-                        date = fn[1:sep]
-                        time = fn[sep+1:]
-                        df = df.append({'Location': loc,'Date': date,'Time':time,'People': ana(fn)}, ignore_index=True)
+    for img in os.listdir('images'):
+        if img.endswith('.jpg'):
+            fn, fext = os.path.splitext(img)
+            if os.path.isfile("pImages\{}new.jpg".format(fn)) == False:
+                loc = fn[0]
+                sep = fn.find('T')
+                date = fn[1:sep]
+                time = fn[sep+1:]
+                df = df.append({'Location': loc,'Date': date,'Time':time,'People': ana(fn)}, ignore_index=True)
 
 df.to_csv('results.csv')
